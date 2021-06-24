f3e49b92b491f0c6fe1d7d9a2563ba30c3d8e126
nurdinghejjr <rimbekovilzar@gmail.com>
2021-06-24 16:37:30 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(" start of  program");
+        kara();
+        kara();
+        kara();
+        kara();
+        System.out.println("End of the program");
+
+
+    }
+
+    public static void kara() {
+        int outside_temperature = 30;
+        int person_age = 25;
+        boolean sunny = true;
+        if (outside_temperature > 20 && person_age >10){
+            System.out.println("можно идти гулять");
+        }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
